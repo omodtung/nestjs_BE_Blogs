03117a4233e638c8ec854f58dc19a2b64c8588c6
omodtung <dothedung131@gmail.com>
2024-08-01 18:37:26 +0700

    Entity user

---
+++ db/data-source.ts
@@ -0,0 +1,17 @@
+import { DataSourceOptions, DataSource } from 'typeorm';
+
+// eslint-disable-next-line prettier/prettier
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: '127.0.0.1',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'blog-nestjs',
+  entities: ['dist/**/*.entity.js'],
+  // migrations: ['dist/db/migrations/*.js'],
+  migrations: ["dist/migrations/**/*{.js,.ts}"],
+  synchronize: false,
+};
+const dataSource = new DataSource(dataSourceOptions);
+export default dataSource;
