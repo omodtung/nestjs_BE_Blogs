f5415cadc2f766d247cab31d9348445e72a110d0
omodtung <dothedung131@gmail.com>
2024-08-02 17:05:45 +0700

    auth service have function generate token , refreshToken , Login , Register

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,10 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user/entities/user.entity';
 import { dataSourceOptions } from 'db/data-source';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule],
+  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule,ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
