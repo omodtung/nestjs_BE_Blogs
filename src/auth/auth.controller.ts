2bb2e3ea99c2bda5ebed50298ac4516924864aaf
omodtung <dothedung131@gmail.com>
2024-08-02 17:07:11 +0700

    auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CreateAuthDto } from './dto/create-auth.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { User } from 'src/user/entities/user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  register(@Body() registerUserDto: RegisterUserDto): Promise<User> {
+    console.log('register api');
+    console.log(registerUserDto);
+    return this.authService.register(registerUserDto);
+  }
+  @Post('login')
+  login(@Body() loginUserDTO: LoginUserDto): Promise<any> {
+    console.log('login api');
+    console.log('refresh_token api');
+    return this.authService.login(loginUserDTO);
+  }
+  @Post('refresh-token')
+  refreshToken(@Body() { refresh_token }): Promise<any> {
+    console.log('refresh token api');
+    return this.authService.refreshToken(refresh_token);
+  }
+}
