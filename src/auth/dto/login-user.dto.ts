6d2ff9ff1d7c622bf7709875641719cc1e8b4bec
omodtung <dothedung131@gmail.com>
2024-08-04 23:40:17 +0700

    dto auth

---
+++ src/auth/dto/login-user.dto.ts
@@ -0,0 +1,14 @@
+// import { ApiProperty } from "@nestjs/swagger";
+import { IsEmail, IsNotEmpty } from "class-validator";
+// import { ApiProperty } from "@nestjs/swagger";
+
+export class LoginUserDto {
+    // @ApiProperty()
+    @IsNotEmpty()
+    @IsEmail()
+    email:string;
+
+    // @ApiProperty()
+    @IsNotEmpty()
+    password:string;
+}
