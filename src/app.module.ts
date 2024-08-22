82ea6b2d89ad2126b34be5e3b71bd7ea30c24ca1
omodtung <dothedung131@gmail.com>
2024-08-22 23:54:15 +0700

    apply RoleGuard

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,32 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { PostModule } from './post/post.module';
 import { CategoryModule } from './category/category.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/roles.guard';
+import { AuthGuard } from './auth/auth.guard';
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions), UserModule, AuthModule,ConfigModule.forRoot(), PostModule, CategoryModule],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceOptions),
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+    PostModule,
+    CategoryModule,
+    TypeOrmModule.forFeature([User])
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+
+      useClass: AuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
